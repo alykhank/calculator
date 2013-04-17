eb79be5ba6af859e4be76a3103cc1baa91f79b14
Alykhan Kanji <github@alykhan.com>
2013-04-17 02:05:59 -0400

    Implement drawRect: for GraphView.

--- Calculator/GraphView.m
+++ Calculator/GraphView.m
@@ -21,13 +21,45 @@
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGPoint origin;
+    origin.x = self.bounds.origin.x + self.bounds.size.width / 2.0;
+    origin.y = self.bounds.origin.y + self.bounds.size.height / 2.0;
+    
+    CGFloat minimumX;
+    minimumX = self.bounds.origin.x - origin.x;
+    CGFloat maximumX;
+    maximumX = self.bounds.size.width - origin.x;
+    
+    CGContextSetLineWidth(context, 1);
+    [[UIColor blueColor] setStroke];
+    
+    CGContextBeginPath(context);
+    CGContextMoveToPoint(context, origin.x + minimumX, origin.y - [self.dataSource yValueForGraphView:self withXValue:minimumX]);
+    
+    if (self.contentScaleFactor == 2.0)
+    {
+        for (float i = minimumX; i <= maximumX; i += 0.5)
+        {
+//            NSLog(@"Mathematical X: %f Y: %f", i, [self.dataSource yValueForGraphView:self withXValue:i]);
+//            NSLog(@"Drawing X: %f Y: %f", origin.x + i, origin.y - [self.dataSource yValueForGraphView:self withXValue:i]);
+            CGContextAddLineToPoint(context, origin.x + i, origin.y - [self.dataSource yValueForGraphView:self withXValue:i]);
+        }
+    }
+    else
+    {
+        for (float i = minimumX; i <= maximumX; i++)
+        {
+//            NSLog(@"Mathematical X: %f Y: %f", i, [self.dataSource yValueForGraphView:self withXValue:i]);
+//            NSLog(@"Drawing X: %f Y: %f", origin.x + i, origin.y - [self.dataSource yValueForGraphView:self withXValue:i]);
+            CGContextAddLineToPoint(context, origin.x + i, origin.y - [self.dataSource yValueForGraphView:self withXValue:i]);
+        }
+    }
+    
+    CGContextStrokePath(context);
 }
-*/
 
 @end
