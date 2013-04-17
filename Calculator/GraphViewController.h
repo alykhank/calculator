60dbba03ca4f71db9f9f1addecc33b32b9c42cd9
Alykhan Kanji <github@alykhan.com>
2013-04-16 21:53:21 -0400

    Add GraphViewController.

---
+++ Calculator/GraphViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.h
+//  Calculator
+//
+//  Created by alykhan.kanji on 16/04/13.
+//  Copyright (c) 2013 Alykhan Kanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphViewController : UIViewController
+
+@end
---
+++ Calculator/GraphViewController.m
@@ -0,0 +1,38 @@
+//
+//  GraphViewController.m
+//  Calculator
+//
+//  Created by alykhan.kanji on 16/04/13.
+//  Copyright (c) 2013 Alykhan Kanji. All rights reserved.
+//
+
+#import "GraphViewController.h"
+
+@interface GraphViewController ()
+
+@end
+
+@implementation GraphViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
