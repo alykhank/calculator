d4475246a4551b7c0190c5db6f9903cc0d5e16fc
Alykhan Kanji <github@alykhan.com>
2015-01-31 02:04:01 -0800

    Create empty Swift project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Alykhan Kanji on 2015-01-31.
+//  Copyright (c) 2015 Alykhan Kanji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
